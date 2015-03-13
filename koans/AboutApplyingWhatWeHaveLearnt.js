febff35dde356a131e9186a9e299df3e569755e1
m-arnold <michaelaarnold@gmail.com>
2015-03-12 19:01:28 -0700

    passed 3rd and 4th tests for AboutApplyingWhatWeHaveLearnt.js

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -66,14 +66,17 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum  = _.chain(_.range(0,1000))    /* try chaining range() and reduce() */
+                .filter(function(x) { return (x % 3 == 0) || (x % 5 == 0) })
+                .reduce(function(a, b) { return a + b })
+                .value();
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
